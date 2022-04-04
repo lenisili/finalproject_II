1556b38c9274030fb57517c9fed35214e9597281
lenisli <lena.simma@gmx.ch>
2022-04-04 20:57:38 +0200

    solved errors

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,10 +7,10 @@ const Home = () => {
     return (
     <>
     <Card className="bg-dark text-white text-center">
-    <Card.Img class="thumbnail" style={{ height: '250px'}} src="https://arnowelzel.de/wp-content/uploads/2017/11/test-dark-640x480.png" />
+    <Card.Img className="thumbnail" style={{ height: '300px'}} src="https://arnowelzel.de/wp-content/uploads/2017/11/test-dark-640x480.png" />
     <Card.ImgOverlay>
-        <Card.Title><h1>kaffeepause cum tempore</h1></Card.Title>
-        <Card.Text><h3>Vom Studentenleben ins Berufsleben: Der Podcast</h3></Card.Text>
+        <Card.Title>kaffeepause cum tempore</Card.Title>
+        <Card.Text>Vom Studentenleben ins Berufsleben: Der Podcast</Card.Text>
         <Card.Text>
         Spaghetti mit Pesto, WG-Partys. Diskussionen über Gott und die Welt: Das Studentenleben hat seine Vorzüge. Und in der Prüfungsphase verfluchen wir es doch manchmal. Aber schnell als man denkt, ist es vorbei und man steht mit beiden Beinen im Berufsleben. Der Wechsel wirkt viele Fragen auf. Den Fragen möchten wir mit den unterschiedlichsten Gesprächspartnern auf den Grund gehen. Lassen wir uns alle inspirieren.
         </Card.Text>
@@ -20,7 +20,7 @@ const Home = () => {
     </Card.ImgOverlay>
     </Card>
     <Card>
-    <Card.Img class="fluid" src="https://images.unsplash.com/photo-1590012314607-cda9d9b699ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdyYWR1YXRpb258ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60" />
+    <Card.Img className="fluid" src="https://images.unsplash.com/photo-1590012314607-cda9d9b699ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdyYWR1YXRpb258ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60" />
     </Card>
     </>
     )
